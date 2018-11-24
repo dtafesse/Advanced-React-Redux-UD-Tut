45c8eeae617fe5aa5badf25ba23056fc29888557
dtafesse <dtafesse@uwm.edu>
2018-11-24 14:57:20 -0600

    comment list test done

--- src/Root.js
+++ src/Root.js
@@ -3,17 +3,13 @@ import { Provider } from "react-redux";
 import { createStore } from "redux";
 import reducers from "reducers/index";
 
-const initalState = {};
-
 // props children - wraps any child components
 // with the parent component
 // in this case - parent - Providor
 // child can be App component for example
 
-export default props => {
+export default ({ children, initalState = {} }) => {
   return (
-    <Provider store={createStore(reducers, initalState)}>
-      {props.children}
-    </Provider>
+    <Provider store={createStore(reducers, initalState)}>{children}</Provider>
   );
 };
