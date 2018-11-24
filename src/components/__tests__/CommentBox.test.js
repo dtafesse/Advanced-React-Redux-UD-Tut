1af3ffa9f7e31a0694258b270c5fbf43ad1f9f9d
dtafesse <dtafesse@uwm.edu>
2018-11-24 12:19:10 -0600

    organization of text area tests

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -21,3 +21,28 @@ it("has a text area and a button", () => {
   expect(wrapped.find("textarea").length).toEqual(1);
   expect(wrapped.find("button").length).toEqual(1);
 });
+
+describe("the text area", () => {
+  beforeEach(() => {
+    // simulate a change event on the textarea,
+    // give it a fake event object, 'mock object',
+
+    wrapped.find("textarea").simulate("change", {
+      target: { value: "new comment" }
+    });
+
+    // since setState is async, have to force component to rerender
+    wrapped.update();
+  });
+
+  it("has a text area that users can type in", () => {
+    // assert the value of textarea has changed
+    expect(wrapped.find("textarea").prop("value")).toEqual("new comment");
+  });
+
+  it("empties the value of the textarea when user submits the form", () => {
+    wrapped.find("form").simulate("submit");
+    wrapped.update();
+    expect(wrapped.find("textarea").prop("value")).toEqual("");
+  });
+});
